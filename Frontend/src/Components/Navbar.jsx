f8b3c61db829bdb30a85bf89f1e7568147e96a55
Shinzo27 <20bmiit031@gmail.com>
2024-05-16 12:07:52 +0530

    Frontend

--- Frontend/src/Components/Navbar.jsx
+++ Frontend/src/Components/Navbar.jsx
@@ -2,27 +2,7 @@ import React from 'react'
 
 const Navbar = () => {
   return (
-    <>
-      <section className="header">
-
-        <img src=".\logo.png" className="logo"/>
-
-        <nav className="navbar">
-        <a href="#home">Home</a>
-        <a href="Product.html">Shop</a>
-        <a href="gallery.html">Gallery</a>
-        <a href="#about">About</a>
-        <a href="#food">Expertise</a>
-        <a href="#blogs">Reviews</a>
-        <a href="#footer">Contact us</a>
-        <a href="Login.html">Login</a>
-        <a href="cart.html">Cart</a>
-        </nav>
-
-        <div id="menu-btn" className="fas fa-bars"></div>
-
-        </section>
-    </>
+    <div>Navbar</div>
   )
 }
 
--- Frontend/src/Pages/Home.jsx
+++ Frontend/src/Pages/Home.jsx
@@ -1,19 +1,9 @@
 import React from 'react'
 import Navbar from '../Components/Navbar'
-import Slider from '../Components/Slider'
-import Aboutus from '../Components/Aboutus'
-import TopProducts from '../Components/TopProducts'
-import Footer from '../Components/Footer'
 
 const Home = () => {
   return (
-    <>
-      <Navbar/>
-      {/* <Slider/> */}
-      <Aboutus/>
-      {/* <TopProducts/>   */}
-      <Footer/>
-    </>
+    <Navbar/>
   )
 }
 
