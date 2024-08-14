aff56e82b246601fe811c03bc01553c4e83dafa9
Shinzo27 <20bmiit031@gmail.com>
2024-08-14 23:57:27 +0530

    Frontend

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,11 +1,14 @@
-import Home from "./Pages/Home"
-import './App.css';
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
 
 function App() {
+  const [count, setCount] = useState(0)
 
   return (
     <>
-      <Home/>
+      <p className='bg-black text-white text-2xl'>Hello world</p>
     </>
   )
 }
