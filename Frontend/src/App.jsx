b7eb63568ae57acf263264a512d5d5d84ee92e21
Shinzo27 <20bmiit031@gmail.com>
2024-09-07 01:35:45 +0530

    Frontend

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,12 +1,14 @@
 import Footer from "./Components/Footer";
 import Header from "./Components/Header";
 import Home from "./Pages/Home";
+import Product from "./Pages/Products";
 
 function App() {
   return (
     <>
       <Header/>
-        <Home/>
+        {/* <Home/> */}
+        <Product/>
       <Footer/>
     </>
   )
---
+++ Frontend/src/Pages/Products.jsx
@@ -0,0 +1,15 @@
+import React, { useEffect, useState } from "react";
+import axios from "axios";
+import ProductCard from "../Components/ProductCard";
+
+const Product = () => {
+//   const [product, setProducts] = useState([]);
+
+  return (
+    <>
+      Product Page!
+    </>
+  );
+};
+
+export default Product;
